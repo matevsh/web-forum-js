425ad37be006d1cf1db6c481daddc36aec38c80c
Mateusz Marcinkowski <mateuszmarcinkowski44@gmail.com>
2022-10-18 19:10:22 +0200

    some mock data

--- packages/backend/src/provider/data.provider.js
+++ packages/backend/src/provider/data.provider.js
@@ -4,6 +4,51 @@ const data = [
         title: 'Co sądzicie o aktualnej sytuacji w Polsce?',
         views: 542,
     },
+    {
+        id: 1,
+        title: 'Podkarpacie. 30-latek kradł pokrywy studzienek kanalizacyjnych. Straty na ponad 70 tys. zł',
+        views: 456,
+    },
+    {
+        id: 2,
+        title: 'Nieoficjalnie: Iran ma dostarczyć Rosji pociski ziemia-ziemia. Umowę zawarto w Moskwie',
+        views: 573,
+    },
+    {
+        id: 3,
+        title: 'Putin zwołał pilne spotkanie Rady Bezpieczeństwa. Już drugie w tym miesiącu',
+        views: 456,
+    },
+    {
+        id: 4,
+        title: 'Incepcja Łukaszenki. Sprawia wrażenie przygotowań do wojny, żeby od tej wojny się wymigać i nie zatonąć z Putinem',
+        views: 122,
+    },
+    {
+        id: 5,
+        title: 'Szymon Hołownia rozlicza PiS. Wystawił faktury. "Ile jeszcze muszą nam ukraść?", "oddajcie 300 mld"',
+        views: 982,
+    },
+    {
+        id: 6,
+        title: '108 Ukrainek za 110 Rosjan. Lekarka z Mariupola, po miesiącach w niewoli, wróciła do 4-letniej córki',
+        views: 2312,
+    },
+    {
+        id: 7,
+        title: 'Oława. "Nikt nikogo za nogę nie trzymał", ale pod betonową płytą studni było ciało. Nowe informacje w sprawie',
+        views: 645,
+    },
+    {
+        id: 8,
+        title: 'Holandia. Pociąg zmiażdżył autobus na przejeździe kolejowym. Przerażające nagranie',
+        views: 231,
+    },
+    {
+        id: 9,
+        title: 'Izrael nie zgodził się na rozmowę z Reznikowem. Kułeba chce zerwania stosunków dyplomatycznych z Iranem',
+        views: 1657,
+    }
 ];
 
 export default data;
